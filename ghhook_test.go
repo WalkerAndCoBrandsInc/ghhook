4b1b3ee4edc5c5302f32e68afe289b42f977f5d2
Senthil Arivudainambi <senthil@walkerandcobrands.com>
2018-03-06 08:14:57 -0800

    refactor tests

--- ghhook_test.go
+++ ghhook_test.go
@@ -9,64 +9,64 @@ import (
 )
 
 func TestEventHandler(t *testing.T) {
-	var fn InputFn = func(e interface{}) (*Response, error) {
-		return nil, nil
-	}
+	Convey("EventHandler", t, func() {
+		var fn InputFn = func(e interface{}) (*Response, error) {
+			return nil, nil
+		}
 
-	Convey("It saves input fn to event name", t, func() {
 		Reset(func() { ResetHandlers() })
 
-		EventHandler(PullRequestEvent, fn)
+		Convey("It saves input fn to event name", func() {
+			EventHandler(PullRequestEvent, fn)
 
-		So(len(Handlers), ShouldEqual, 1)
-		So(len(Handlers[PullRequestEvent]), ShouldEqual, 1)
-		So(Handlers[PullRequestEvent][0], ShouldEqual, fn)
-	})
+			So(len(Handlers), ShouldEqual, 1)
+			So(len(Handlers[PullRequestEvent]), ShouldEqual, 1)
+			So(Handlers[PullRequestEvent][0], ShouldEqual, fn)
+		})
 
-	Convey("It appends input fn to event name", t, func() {
-		Reset(func() { ResetHandlers() })
+		Convey("It appends input fn to event name", func() {
+			EventHandler(PullRequestEvent, fn)
+			EventHandler(PullRequestEvent, fn)
 
-		EventHandler(PullRequestEvent, fn)
-		EventHandler(PullRequestEvent, fn)
-
-		So(len(Handlers[PullRequestEvent]), ShouldEqual, 2)
-		So(Handlers[PullRequestEvent][0], ShouldEqual, fn)
-		So(Handlers[PullRequestEvent][1], ShouldEqual, fn)
+			So(len(Handlers[PullRequestEvent]), ShouldEqual, 2)
+			So(Handlers[PullRequestEvent][0], ShouldEqual, fn)
+			So(Handlers[PullRequestEvent][1], ShouldEqual, fn)
+		})
 	})
 }
 
 func TestDefaultHandler(t *testing.T) {
-	Convey("It drops unregistered events", t, func() {
+	Convey("DefaultHandler", t, func() {
 		Reset(func() { ResetHandlers() })
 
-		resp, err := DefaultHandler(PullRequestProxyRequest)
-		So(err, ShouldBeNil)
+		Convey("It drops unregistered events", func() {
+			resp, err := DefaultHandler(PullRequestProxyRequest)
+			So(err, ShouldBeNil)
 
-		So(resp, ShouldNotBeNil)
-		So(resp.StatusCode, ShouldEqual, 200)
-		So(resp.Body, ShouldEqual, "")
-	})
+			So(resp, ShouldNotBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			So(resp.Body, ShouldEqual, "Dropping unregistered event: 'pull_request'")
+		})
 
-	Convey("It calls registered fn for event", t, func() {
-		Reset(func() { ResetHandlers() })
+		Convey("It calls registered fn for event", func() {
+			var fn InputFn = func(e interface{}) (*Response, error) {
+				pr, ok := e.(*github.PullRequestEvent)
+				So(ok, ShouldBeTrue)
 
-		var fn InputFn = func(e interface{}) (*Response, error) {
-			pr, ok := e.(*github.PullRequestEvent)
-			So(ok, ShouldBeTrue)
+				return &Response{
+					Body:       fmt.Sprintf("%s", *pr.Action),
+					StatusCode: 200,
+				}, nil
+			}
 
-			return &Response{
-				Body:       fmt.Sprintf("%s", *pr.Action),
-				StatusCode: 200,
-			}, nil
-		}
+			EventHandler(PullRequestEvent, fn)
 
-		EventHandler(PullRequestEvent, fn)
+			resp, err := DefaultHandler(PullRequestProxyRequest)
+			So(err, ShouldBeNil)
 
-		resp, err := DefaultHandler(PullRequestProxyRequest)
-		So(err, ShouldBeNil)
-
-		So(resp, ShouldNotBeNil)
-		So(resp.StatusCode, ShouldEqual, 200)
-		So(resp.Body, ShouldEqual, "opened")
+			So(resp, ShouldNotBeNil)
+			So(resp.StatusCode, ShouldEqual, 200)
+			So(resp.Body, ShouldEqual, "opened")
+		})
 	})
 }
